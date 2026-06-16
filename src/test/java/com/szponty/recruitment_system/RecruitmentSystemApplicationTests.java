79ec826118507d24fe2a33037516bb3bfb4d0810
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2026-06-16 20:48:33 +0000

    feat: initial template

---
+++ src/test/java/com/szponty/recruitment_system/RecruitmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szponty.recruitment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
