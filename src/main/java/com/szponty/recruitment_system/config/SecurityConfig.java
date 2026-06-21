da9dcd587bd2990671e7ecc6e4a1a2dfe8f16852
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2026-06-21 15:00:41 +0000

    feat: disabled security temporary

---
+++ src/main/java/com/szponty/recruitment_system/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.szponty.recruitment_system.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .anyRequest().permitAll()  //  security temporary disabled - all requests were returned with 401/403 code
+                )
+                .build();
+    }
+}
