10d1af0c42575a5e0aa00f4f95fcb6ce2ab9d46c
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2026-06-24 21:09:41 +0000

    feat: changed UserCreatedEvent, added UserUpdatedEvent

--- src/main/java/com/szponty/recruitment_system/event/model/UserUpdatedEvent.java
+++ src/main/java/com/szponty/recruitment_system/event/model/UserUpdatedEvent.java
@@ -4,11 +4,8 @@ import com.szponty.recruitment_system.dictionary.model.Department;
 import com.szponty.recruitment_system.role.model.Role;
 import com.szponty.recruitment_system.user.model.User;
 
-public record UserCreatedEvent(
+public record UserUpdatedEvent(
         User createdBy,
-        String firstName,
-        String lastName,
-        String email,
         Role role,
         Department department
 ) {
