2a22c75481449998486b98674ac02b840a1c2532
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2026-07-06 17:12:24 +0000

    feat: added job offer repo

---
+++ src/main/java/com/szponty/recruitment_system/joboffer/repository/JobOfferRepository.java
@@ -0,0 +1,46 @@
+package com.szponty.recruitment_system.joboffer.repository;
+
+import com.szponty.recruitment_system.joboffer.model.JobOffer;
+import com.szponty.recruitment_system.joboffer.model.JobOfferStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JobOfferRepository extends JpaRepository<JobOffer, UUID> {
+    List<JobOffer> findAllByStatusActive();
+
+    @Query("""
+        SELECT jobOffer
+        FROM JobOffer jobOffer
+          WHERE jobOffer.offerStatus = :status
+          AND jobOffer.validFrom <= :now
+          AND jobOffer.validTo >= :now
+    """)
+    List<JobOffer> findPublicOffers(
+            @Param("status") JobOfferStatus status,
+            @Param("now") LocalDateTime now
+    );
+
+    @Query("""
+        SELECT jobOffer
+        FROM JobOffer jobOffer
+        WHERE jobOffer.id = :id
+          AND jobOffer.offerStatus = :status
+          AND jobOffer.validFrom <= :now
+          AND jobOffer.validTo >= :now
+    """)
+    Optional<JobOffer> findPublicOfferById(
+            @Param("id") UUID id,
+            @Param("status") JobOfferStatus status,
+            @Param("now") LocalDateTime now
+    );
+
+    List<JobOffer> findAllByDeletedFalse();
+
+    Optional<JobOffer> findByIdAndDeletedFalse(UUID id);
+}
