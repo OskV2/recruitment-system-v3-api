e08a3b60c115669742dd633bbf277c64dad77cb6
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2026-07-13 19:10:56 +0000

    Reapply "feat: extracted logic of helpers from service into dedicated resolver"
    This reverts commit b68c358f4dfa3df3c40faef0ac9252bbd8e52a95.

--- src/main/java/com/szponty/recruitment_system/dictionary/repository/ContractTypeRepository.java
+++ src/main/java/com/szponty/recruitment_system/dictionary/repository/ContractTypeRepository.java
@@ -2,5 +2,6 @@ package com.szponty.recruitment_system.dictionary.repository;
 
 import com.szponty.recruitment_system.dictionary.model.ContractType;
 
-public interface ContractTypeRepository extends DictionaryRepository<ContractType> {
+public interface ContractTypeRepository
+        extends DictionaryRepository<ContractType>{
 }
