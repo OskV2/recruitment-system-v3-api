f9436247f10e5770bd4dd5b67285564125058173
Maciej Buszkiewicz <maciej.buszkiewicz1@gmail.com>
2026-07-30 22:35:56 +0000

    feat: moved mermaid schema to separate file

--- src/test/java/com/szponty/recruitment_system/dictionary/service/BenefitServiceTest.java
+++ src/test/java/com/szponty/recruitment_system/dictionary/service/BenefitServiceTest.java
@@ -9,11 +9,9 @@ import com.szponty.recruitment_system.dictionary.repository.BenefitRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
