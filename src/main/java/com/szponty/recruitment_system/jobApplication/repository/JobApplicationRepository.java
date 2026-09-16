dfb7e6129683197f5bead4d5a17ebfeef58a2dd8
Marcin Tyszer <marcin.tyszer@protonmail.com>
2026-09-16 20:18:26 +0200

    Revert "feat: added JobApplication service layer with filtering and creation"
    This reverts commit c32acfa1205847098ac3e1c5a3f0b12f18a9002b.

--- src/main/java/com/szponty/recruitment_system/jobApplication/repository/JobApplicationRepository.java
+++ src/main/java/com/szponty/recruitment_system/jobApplication/repository/JobApplicationRepository.java
@@ -2,22 +2,8 @@ package com.szponty.recruitment_system.jobApplication.repository;
 
 import com.szponty.recruitment_system.common.repository.FindOrThrowRepository;
 import com.szponty.recruitment_system.jobApplication.model.JobApplication;
-import com.szponty.recruitment_system.jobApplication.model.JobApplicationStatus;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface JobApplicationRepository extends FindOrThrowRepository<JobApplication, UUID> {
-    @Query("""
-        SELECT ja FROM JobApplication ja
-        WHERE ja.deleted = false
-        AND (:jobOfferId IS NULL OR ja.jobOffer.id = :jobOfferId)
-        AND (:status IS NULL OR ja.status = :status)   
-    """)
-    List<JobApplication> findAllByOptionalFilters(
-            @Param("jobOfferId") UUID jobOfferId,
-            @Param("status") JobApplicationStatus status
-    );
 }
